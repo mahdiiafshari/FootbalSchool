2cab1607c8d277a348abf1330b1f3ef450599d87
mahdiiafshari <mahdiafshari661384@gmail.com>
2025-08-19 23:14:31 +0330

    EXT : creat player model

--- FootbalSchool/settings.py
+++ FootbalSchool/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     "account",
     "manager",
     "school",
+    "player",
 
 ]
 
--- player/models.py
+++ player/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from manager.models import Manager
+from school.models import School
 
-# Create your models here.
+
+User=get_user_model()
+
+class Player(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE ,related_name='player')
+    school = models.ForeignKey(School,on_delete=models.CASCADE,related_name='player')
+    jersey_number = models.PositiveIntegerField(null=True,blank=True)
+    manager = models.ForeignKey(Manager, on_delete=models.SET_NULL, null=True,related_name='player')
+
+    def __str__(self):
+        return str(self.user)
